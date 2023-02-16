eb7d25009932159ac05778c0428296deb3de6dff
Ahmed Helmy <ah8762853@gmail.com>
2023-02-16 11:46:49 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 1 (Error code)
+ */
+int main(void)
+{
+	const char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	ssize_t len = sizeof(str) - 1;
+	ssize_t written = write(STDERR_FILENO, str, len);
+	return (written != len);
+}
