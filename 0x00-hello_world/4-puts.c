ee22bab7a308941a312fb237b2f18f61a76056df
Ahmed Helmy <ah8762853@gmail.com>
2023-02-16 10:14:39 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * print_hello - Prints "Hello"
+ * Return:0 (seccess)
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
