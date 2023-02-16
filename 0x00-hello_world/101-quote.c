d600660414dc23194afbb4e13c9022a153319371
Ahmed Helmy <ah8762853@gmail.com>
2023-02-16 12:04:46 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,10 +7,6 @@
  */
 int main(void)
 {
-	const char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	ssize_t len = sizeof(str) - 1;
-	ssize_t written = write(STDERR_FILENO, str, len);
-	if (written != len)
-		return (1);
-			return (0);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
