335f40424530637ea6a1b33cae008232fa358367
Ahmed Helmy <ah8762853@gmail.com>
2023-02-16 09:15:50 -0800

    4-puts.c

---
+++ 0x00-hello_world/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char m,l,s;
+	int age ;
+	scanf("%c %c %c %d",&m,&l,&s,&age);
+	printf("My intials are: %c%c%c and my age is: %d.\n", m, l, s,age);
+	return 0;
+}
