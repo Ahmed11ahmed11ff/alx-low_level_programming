5c11e95d9e720a3b848a1c61d15b83ca3507dd03
Ahmed Helmy <ah8762853@gmail.com>
2023-02-16 11:52:38 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,5 +10,7 @@ int main(void)
 	const char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	ssize_t len = sizeof(str) - 1;
 	ssize_t written = write(STDERR_FILENO, str, len);
-	return (written != len);
+	if (written != len)
+		return (1);
+			return (0);
 }
