4ed838f6c4a0cc0ddc1720a3276ef2c501f923c9
Ahmed Helmy <ah8762853@gmail.com>
2023-02-17 09:22:03 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,13 +9,13 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i < 8;  i++)
+	for (i = 0; i <= 8;  i++)
 	{
-		for (j = 1; j < 9; j++)
+		for (j = 1; j <= 9; j++)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
-			if (j != 8 || i != 9)
+			if (j < 9 || i <8)
 			{
 				putchar(',');
 				putchar(' ');
