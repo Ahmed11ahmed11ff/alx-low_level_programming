3c30517dd7ba175b91ab75db9664de947e725d1c
Ahmed Helmy <ah8762853@gmail.com>
2023-02-21 10:27:15 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/**
+ * print_alphabets - prints alphabet, in lowercase
+ */
+
+void print_alphabet(void);
+{
+	char count;
+
+	for (count = 'a'; count <= 'z'; count++)
+	{
+		_putchar(count);
+	}
+	_puchar('\n');
+}
