1e7d2cb128b83f4dda2de04524b58ecccc5aafe6
Ahmed Helmy <ah8762853>
2023-02-21 15:44:51 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,40 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints the first 98 Fibonacci numbers.
  *
- * Description: print the first 98 numbers of Fibonacci
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 
-void print_times_table(int n)
+int main(void)
 {
-	if (n >= 98)
+	unsigned long int i, bef = 1, aft = 2, bef1, bef2, aft1, aft2, l = 1000000000;
+
+	printf("%lu", bef);
+
+	for (i = 1; i < 91; i++)
 	{
-		while(n > 98)
-		{
-			printf("%d, ", n--);
-		}
-		printf("%d\n", n);
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
 	}
-	else
+
+	bef1 = bef / l;
+	bef2 = bef % l;
+	aft1 = aft / l;
+	aft2 = aft % l;
+
+	for (i = 92; i < 99; i++)
 	{
-		while (n < 98)
-		{
-			printf("%d, ", n++);
-		}
-		printf("%d\n", n);
+		printf(", %lu", aft1 + (aft2 / l));
+		printf("%0*lu", (int)(9 - (aft2 == 0)) , aft2);
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
 	}
+
+	printf("\n");
+
+	return (0);
 }
