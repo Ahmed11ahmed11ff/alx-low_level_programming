bb826f7cda0179f8a6e33d4d61028fa06b2ace08
Ahmed Helmy <ah8762853>
2023-02-24 13:26:02 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers from 1 to 9, expect 2 and 4.
+ *
+ * return: void
+ */
+
+print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i != 52 || i != 50)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
