a198c0dded05d695e03565214fc239a490d77eae
Ahmed Helmy <ah8762853>
2023-02-24 14:09:14 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- * more_numbers - prints the numbers from 0 to 14, 10 times.
- *
- * return: void.
+ * more_numbers - prints 10 times the numbers, from 0 to 14,
+ * followed by a new line
  */
-
 void more_numbers(void)
 {
 	int i;
