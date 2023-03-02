eef4e9e7c596a0b08a709d2ddac7e592ef0837e0
Ahmed Helmy <ah8762853>
2023-03-02 09:35:43 -0800

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-int n;
-int a[5];
-int *p;
+	int n;
+	int a[5];
+	int *p;
 
-a[2] = 1024;
-p = &n;
-*(a + 2) = 98;
-printf("a[2] = %d\n", a[2]);
-return (0);
+	a[2] = 1024;
+	p = &n;
+	/**
+	 * Add one line here to make the program print 98
+	 * and follow the rules described in the prompt
+	 */
+	*(p + 5) = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
-
