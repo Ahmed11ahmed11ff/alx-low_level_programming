5365f3f23f10a603a1f1753519605feec6a14388
Ahmed Helmy <ah8762853>
2023-03-02 05:33:07 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,21 +11,5 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int len = 0;
-	int i = 0;
-
-	while (dest[len] != '\0')
-	{
-		len++;
-	}
-
-	while (src[i] != '\0')
-	{
-		dest[len + i] = src[i];
-		i++;
-	}
-
-	dest[len + i] = '\0';
-
-	return (dest);
+	return (strcat(dest, src));
 }
