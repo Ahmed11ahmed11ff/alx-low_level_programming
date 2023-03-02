2a4cd606ab176a44d698856c613020eef193d866
Ahmed Helmy <ah8762853>
2023-03-02 09:31:16 -0800

    102 maigic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
-#include "main.h"
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 
 int main(void)
 {
-	int a[5] = {98, 102, 402, -1024, 98};
-	int *p;
+int n;
+int a[5];
+int *p;
 
-	p = &a[2];
-	*(p + 1) = 98;
-
-	printf("a[2] = %d\n", *(a + 2));
-	
-	return (0);
+a[2] = 1024;
+p = &n;
+*(a + 2) = 98;
+printf("a[2] = %d\n", a[2]);
+return (0);
 }
 
