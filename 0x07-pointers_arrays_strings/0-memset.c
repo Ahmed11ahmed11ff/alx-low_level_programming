c0652005cf2be25e6977c35bcdf2b519c087000f
Ahmed Helmy <ah8762853>
2023-03-06 02:50:17 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,11 +12,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
-	for (i = 0; i < n; i++)
+	for (; n > 0;)
 	{
 		s[i] = b;
+		n--;
 	}
 	return (s);
 }
