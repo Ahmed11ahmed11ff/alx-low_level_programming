cd9851590980c9c1ab732fa6d82685fec5037511
Ahmed Helmy <ah8762853>
2023-03-10 12:33:28 -0800

    make liberary

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+#include<stdio.h>
+/**
+ * _puts -Print a statement
+ *
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+	puts("\"Programming is like building a multilingual puzzlei");
+	return (0);
+}
