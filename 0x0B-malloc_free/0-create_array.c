c86631901a069643e1598d15f8b00ccaaae925cf
Ahmed Helmy <ah8762853>
2023-03-16 10:50:44 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - Function that creates an array of chars
+ * , and initializes it with a specific char.
+ *
+ * @size: Size of array.
+ * @c: Char to assign.
+ *
+ * return: Pointer to array, NULL if fail.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i;
+
+	if (size == 0 || str == NULL)
+
+		return (NULL);
+
+	for (i = 0 ; i < size; i++)
+	{
+		str[i] = c;
+	}
+	return (str);
+}
--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -25,12 +25,14 @@ int count_word(char *s)
 
 	for (i = 1; i < len; i++)
 	{
-        if (s[i] == ' ' && s[i - 1] != ' ') {
-            count++;
-        }
-    }
+		if (s[i] == ' ' && s[i - 1] != ' ')
+		{
+			count++;
+		
+		}
+	}
 
-    return count;
+	return (count);
 }
 
 /**
@@ -42,44 +44,52 @@ int count_word(char *s)
  */
 char **strtow(char *str)
 {
-    char **matrix;
-    int count, i, j, k, len, m;
-    char *token;
+	char **matrix;
+       	int count, i, j, k, len, m;
+	char *token;
+	
+	if (str == NULL)
+	{
+		return NULL;
+	}
 
-    if (str == NULL) {
-        return NULL;
-    }
+	count = count_word(str);
+	if (count == 0)
+	{
+		return NULL;
+	}
 
-    count = count_word(str);
-    if (count == 0) {
-        return NULL;
-    }
+	matrix = malloc(sizeof(char *) * (count + 1));
+	if (matrix == NULL)
+	{
+		return NULL;
+	}
 
-    matrix = malloc(sizeof(char *) * (count + 1));
-    if (matrix == NULL) {
-        return NULL;
-    }
+	for (i = 0, k = 0; i < count; i++, k++)
+	{
+		while (str[k] == ' ')
+		{
+			k++;
+		}
+		len = 0;
+		for (j = k; str[j] != ' ' && str[j] != '\0'; j++, len++)
+		{}
+		token = malloc(sizeof(char) * (len + 1));
+		if (token == NULL)
+		{
+			for (m = 0; m < i; m++)
+			{
+				free(matrix[m]);
+			}
+			free(matrix);
+			return (NULL);
+		}
+			strncpy(token, &str[k], len);
+			token[len] = '\0';
+			matrix[i] = token;
+			k = j;
+	}
 
-    for (i = 0, k = 0; i < count; i++, k++) {
-        while (str[k] == ' ') {
-            k++;
-        }
-        len = 0;
-        for (j = k; str[j] != ' ' && str[j] != '\0'; j++, len++) {}
-        token = malloc(sizeof(char) * (len + 1));
-        if (token == NULL) {
-            for (m = 0; m < i; m++) {
-                free(matrix[m]);
-            }
-            free(matrix);
-            return NULL;
-        }
-        strncpy(token, &str[k], len);
-        token[len] = '\0';
-        matrix[i] = token;
-        k = j;
-    }
-
-    matrix[count] = NULL;
-return matrix;
+	matrix[count] = NULL;
+return (matrix);
 }
